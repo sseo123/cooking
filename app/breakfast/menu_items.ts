0544e8dc225796b42373abd22f8d3a540d045c33
bigbosscreator123 <shawnseo123@gmail.com>
2025-10-15 20:20:57 -0700

    allowed user to see the video and image of the desired meal. Code is sloppy currently, I will fix this in my next commit

--- app/breakfast/menu_items.ts
+++ app/breakfast/menu_items.ts
@@ -1,11 +1,15 @@
 export const breakfastItems = [  
 {   name: "pancakes",
     ingredients: ["flour", "milk", "eggs", "butter"],
-    steps: ["Mix ingredients", "Cook on griddle", "Serve warm"] 
+    steps: ["Mix ingredients", "Cook on griddle", "Serve warm"],
+    image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bhg.com%2Frecipe%2Fpancakes-waffles%2Fpancakes%2F&psig=AOvVaw0rvBXvkKDpUQLiJ1kIaRkD&ust=1760662596250000&source=images&cd=vfe&opi=89978449&ved=0CBYQjRxqFwoTCMDFg7fBp5ADFQAAAAAdAAAAABAE",
+    video: "https://www.youtube.com/embed/NCMKedZvnyI?si=YpTdQh-p5zX-3bVl"
 },
 
 {   name: "oatmeal",
     ingredients: ["oats", "milk", "honey", "fruit"],
-    steps: ["Boil milk", "Add oats", "Simmer and serve"]
+    steps: ["Boil milk", "Add oats", "Simmer and serve"],
+    image: "https://cdn.loveandlemons.com/wp-content/uploads/2019/09/oatmeal-recipe.jpg",
+    video: "https://www.youtube.com/embed/JzsNQOwI-sA?si=0xPCuhp7Fij9gfUB"
 }
 ]
--- app/breakfast/page.tsx
+++ app/breakfast/page.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 import { breakfastItems } from "../breakfast/menu_items";
 
 export default function Breakfast() {
-    const[breakfast, setBreakfast] = useState("");
+    const[breakfast, setBreakfast] = useState<string | null>(null);
     const[selectedDish, setSelectedDish] = useState<string | null>(null);
 
-    const menuItems = ["pancakes", "oatmeal", "sassuage", "eggs"];
+    const menuItems = ["pancakes", "oatmeal", "sausage", "eggs"];
 
     const randomBreakfastItem = () => {
         const randomItem = menuItems[Math.floor(Math.random() * menuItems.length)];
@@ -28,19 +28,32 @@ export default function Breakfast() {
             <div
             style={{
                 display: "flex",
+                flexDirection: "column",
                 alignItems: "center",
                 justifyContent: "center",
-                marginTop: 100
-            }}> <button onClick={handleDishClick}>{breakfast}</button> 
+                marginTop: 100,
+                gap: "100px"
+            }}> 
+            <button onClick={randomBreakfastItem}>Generate New</button>
+            <button onClick={handleDishClick}><strong>{breakfast}</strong></button> 
             </div> 
-                <div style={{ position: "relative", top: 400, display: "flex", justifyContent: "center"}}>
-                    <button onClick={randomBreakfastItem}>Generate New</button>
-                </div>
+     
             <div style={{ position: "absolute", left: 100, top: 30 }}>
                 <Link href="/"><button>Back to home</button></Link>
             </div>
             {matchDishName && (
                 <div style={{ display: "flex", justifyContent: "center", alignItems: "flex-start", gap: "100px", marginTop: "60px", textAlign: "left" }}>
+                         
+                    <div><strong>video tutorial:</strong>
+                        <iframe 
+                        width="560" 
+                        height="315" 
+                        src={matchDishName.video}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                        allowFullScreen>
+                        </iframe>
+                   </div>
+                   
                     <div> <strong>Ingredients:</strong>
                         {matchDishName.ingredients.map((step, i) => (
                             <ol key={i}>{step}</ol> ))}
@@ -49,6 +62,15 @@ export default function Breakfast() {
                         {matchDishName.steps.map((step, i) => (
                             <ol key={i}>{step}</ol>))}
                     </div>
+                    
+                    <div>
+                        <img
+                        width="560" 
+                        height="315" 
+                        src={matchDishName.image} >
+                        </img>
+                    </div>
+                    
                 </div>
             )}
         </>
